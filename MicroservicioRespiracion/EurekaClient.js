162ea6e59527d059351be50ba1ef283f1eddfc34
carlosrmngucuenca <carlos.munozg@ucuenca.edu.ec>
2022-08-03 23:03:10 -0500

    agrgados los archivos conf

---
+++ MicroservicioRespiracion/EurekaClient.js
@@ -0,0 +1,25 @@
+import Eureka from 'eureka-js-client';
+ 
+// Or, if you're not using a transpiler:
+const Eureka = require('eureka-js-client').Eureka;
+ 
+// example configuration
+const client = new Eureka({
+  // application instance information
+  instance: {
+    app: 'Respiracionqservice',
+    hostName: 'localhost',
+    ipAddr: '127.0.0.1',
+    port: 3750,
+    vipAddress: 'jq.test.something.com',
+    dataCenterInfo: {
+      name: 'MyOwn',
+    },
+  },
+  eureka: {
+    // eureka server host / port
+    // http://52.73.98.2:8099/eureka/
+    host: '52.73.98.2',
+    port: 8099,
+  },
+});
